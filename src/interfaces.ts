5f112a19d55892d440c5d3050911c93eddd196b2
prince curie <prince.chibuike@gmail.com>
2022-01-24 09:05:25 +0100

    create interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,21 @@
+interface IToken {
+    id: string,
+    __typename: string
+}
+
+interface IPair {
+    id: string,
+    token0: IToken,
+    token1: IToken,
+    __typename: string
+}
+
+export interface IData {
+    pairs: IPair[]
+}
+
+export interface IResultItem {
+    pair_id: string,
+    token0_id: string,
+    token1_id: string
+}
