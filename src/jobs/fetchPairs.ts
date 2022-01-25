0cc8e344f4bbfce36b57e394398d20dbb0277e35
prince curie <prince.chibuike@gmail.com>
2022-01-25 05:29:39 +0100

    Move the code for running a scheduler into a class

--- src/jobs/fetchPairs.ts
+++ src/jobs/fetchPairs.ts
@@ -1,69 +1,100 @@
 import { schedule, ScheduledTask } from "node-cron";
 import ApolloClient, { ApolloQueryResult } from "apollo-boost";
+import { DataProxy } from "apollo-cache";
 import gql from "graphql-tag";
 import "cross-fetch/polyfill";
 import db from "../database";
 import { config } from "dotenv";
-import { IData, IResultItem } from "../interfaces";
+import { IData, IResultItem, IPair } from "../interfaces";
 import logger from "../logger";
 
 config();
 
-const client = new ApolloClient({
-    uri: process.env.UNISWAP_BASE_URL
-});
-
-const fetchPairs:ScheduledTask = schedule('*/1 * * * *', async () => {
-    let lastId:string|null = '';
-    let result:IResultItem[] = []
+class FetchPairs {
+    private client:ApolloClient<DataProxy>;
     
-    try {
-        while(lastId !== null) {
-            const {data}:ApolloQueryResult<IData> = await client.query({
-                query: gql(`
-                    {
-                        pairs(where: {id_gt: "${lastId}"}, first: 1000) {
+    constructor() {
+        this.client = new ApolloClient({
+            uri: process.env.UNISWAP_BASE_URL
+        });
+    }
+    
+    private async fetchFromSubgraph(lastId?:string):Promise<IData> {
+        const { data }:ApolloQueryResult<IData> = await this.client.query({
+            query: gql(`
+                {
+                    pairs(where: {id_gt: "${lastId}"}, first: 1000) {
+                        id
+                        token0{
                             id
-                            token0{
-                                id
-                            }
-                            token1{
-                                id
-                            }
+                        }
+                        token1{
+                            id
                         }
                     }
-            `)})
+                }
+        `)})
 
-            let length:number = data.pairs.length
-
-            if(length > 0) {
-                let formattedData:IResultItem[] = data.pairs.map(datum => {
-                    return {
-                        pair_id: datum.id,
-                        token0_id: datum.token0.id,
-                        token1_id: datum.token1.id
-                    }
-                })
-                result.push(...formattedData)
-            }
-
-            lastId = data.pairs[length - 1] ? 
-                data.pairs[length - 1].id : 
-                null;
-            
-            if(lastId === null) {
-                await db('pairs').insert(result)
-                    .onConflict('pair_id')
-                    .ignore();
-            };
-        }
-    } catch (error) {
-        logger.info(error);
+        return data;
     }
-}, {
-    scheduled: true        
-})
 
-fetchPairs.start()
+    private async getAllPairs():Promise<IPair[]> {
+        let allPairs:IPair[] = [];
+        let lastId:string|null = '';
 
-export default fetchPairs;
+        while(lastId !== null) {
+            let {pairs}:IData = await this.fetchFromSubgraph(lastId)
+            let length:number = pairs.length
+
+            if(length > 0) allPairs.push(...pairs)
+
+            lastId = pairs[length - 1] ? pairs[length - 1].id : null;
+        }
+
+        return allPairs;
+    }
+
+    private async formatPairs(allPairs:IPair[]):Promise<IResultItem[]> {
+        let result:IResultItem[] = allPairs.map(pair => {
+            return {
+                pair_id: pair.id,
+                token0_id: pair.token0.id,
+                token1_id: pair.token1.id
+            }
+        });
+        return result;
+    }
+    
+    private async insertToDb (data:IResultItem[]) {
+
+        const trx = await db.transaction()
+
+        await trx('pairs').insert(data)
+            .onConflict('pair_id')
+            .merge();
+        
+        await trx.commit();
+    }
+
+    public async start() {
+        const fetchPairs:ScheduledTask = schedule('*/5 * * * *', async () => {
+            try {
+                logger.info('Started')
+
+                let allPairs:IPair[] = await this.getAllPairs();
+                let formattedPairs:IResultItem[] = await this.formatPairs(allPairs);
+                await this.insertToDb(formattedPairs);
+
+                logger.info('Stopped')
+            } catch (error) {
+                throw error;
+            }
+        }, {
+            scheduled: true        
+        })
+        
+        fetchPairs.start();
+    }
+}
+
+export default new FetchPairs();
